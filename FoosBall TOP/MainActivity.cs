0fa6640721e5005e3ae58bbb7435ef9fa78b5a8c
Karolis <karolis.ziu@gmail.com>
2017-09-14 14:19:34 +0300

    basic UI

--- FoosBall TOP/MainActivity.cs
+++ FoosBall TOP/MainActivity.cs
@@ -1,18 +1,30 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Com.Lilarcor.Cheeseknife;
 
 namespace FoosBall_TOP
 {
     [Activity(Label = "FoosBall_TOP", MainLauncher = true)]
     public class MainActivity : Activity
     {
+
+        [InjectView(Resource.Id.game_video)] VideoView gameVideoView;
+        [InjectView(Resource.Id.blue_team)] TextView blueTeamTextView;
+        [InjectView(Resource.Id.red_team)] TextView redTeamTextView;
+        [InjectView(Resource.Id.event_log)] TextView eventLogTextView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            Cheeseknife.Inject(this);
+            loadVideo();
+        }
+
+        public void loadVideo()
+        {
+            gameVideoView.SetVideoPath("http://techslides.com/demos/sample-videos/small.mp4");
+            gameVideoView.Start();
         }
     }
 }
